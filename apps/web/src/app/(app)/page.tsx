3db8ace9b1936af0543f95d9acf24d7a9f2dfd23
souorichard <rodrigues.lpta@gmail.com>
2025-03-11 22:37:29 -0300

    feat: parallel and interception routes in Next.js

--- apps/web/src/app/(app)/page.tsx
+++ apps/web/src/app/(app)/page.tsx
@@ -1,3 +1,12 @@
+import { Header } from '@/components/header'
+
 export default async function Home() {
-  return <h1 className="text-2xl font-bold">Select an organization</h1>
+  return (
+    <div className="space-y-4 py-4">
+      <Header />
+      <main className="mx-auto w-full max-w-[1200px] space-y-4">
+        Select an organization
+      </main>
+    </div>
+  )
 }
